323c669a0c1a4b4632167de3dc1142aa0559e5df
Daniela Munoz <danielamun.832@gmail.com>
2022-04-26 12:48:41 -0500

    correcting-map-style

---
+++ js/script.js
@@ -0,0 +1,8 @@
+function initpage() {
+    var pg = document.getElementById('page');
+    var t = document.getElementById('ti');
+    pg.addEventListener('click', function(){
+      alert("You're on the " + t.value + " page.");
+    });
+   }
+   window.addEventListener('load', initpage);
